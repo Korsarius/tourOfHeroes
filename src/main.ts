b8e95ee19bb81fb0332826878955bf6849bf649f
Alexey Lomov <korsarius846@gmail.com>
2021-03-05 16:34:32 +0200

    initiation app project in angular

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  console.log('Hello');
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
