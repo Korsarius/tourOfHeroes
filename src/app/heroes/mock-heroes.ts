74d0bee56938faa74a21284f89068ea922ade241
Alexey Lomov <korsarius846@gmail.com>
2021-03-07 19:02:29 +0200

    heroes was changed

--- src/app/heroes/mock-heroes.ts
+++ src/app/heroes/mock-heroes.ts
@@ -1,14 +1,63 @@
 import { IHero } from './IHero';
 
 export const HEROES: IHero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
+  {
+    id: 11,
+    name: 'Superman',
+    photo:
+      'https://www.lego.com/cdn/cs/set/assets/blt6c65eb54dc9fb50e/DC_-_Character_-_Details_-_Sidekick-Standard_-_Superman.jpg?fit=crop&format=jpg&quality=80&width=800&height=426&dpr=1',
+  },
+  {
+    id: 12,
+    name: 'Ironman',
+    photo:
+      'https://sabrina.biz.ua/19508-superlarge_default/tancuyusshij-zheleznyj-chelovek-geroi-dance-hero-ironman-pa10-8.jpg',
+  },
+  {
+    id: 13,
+    name: 'Batman',
+    photo:
+      'https://cdn.pocket-lint.com/r/s/320x/assets/images/149433-tv-feature-what-order-should-you-watch-the-batman-movies-and-shows-image10-nmizadlas2.jpg?v1',
+  },
+  {
+    id: 14,
+    name: 'Black Panther',
+    photo:
+      'https://i.pinimg.com/originals/34/a9/62/34a9624e3658bab0e1effbac9df7b01e.jpg',
+  },
+  {
+    id: 15,
+    name: 'Captain America',
+    photo:
+      'https://upload.wikimedia.org/wikipedia/ru/thumb/4/4d/%D0%9A%D0%B0%D0%BF%D0%B8%D1%82%D0%B0%D0%BD_%D0%90%D0%BC%D0%B5%D1%80%D0%B8%D0%BA%D0%B0.jpg/250px-%D0%9A%D0%B0%D0%BF%D0%B8%D1%82%D0%B0%D0%BD_%D0%90%D0%BC%D0%B5%D1%80%D0%B8%D0%BA%D0%B0.jpg',
+  },
+  {
+    id: 16,
+    name: 'Spider-Man',
+    photo:
+      'https://s.yimg.com/uu/api/res/1.2/dBkcufjWEf1ZLHDw6maxdw--~B/aD02NTA7dz0xMjAwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en-US/fatherly_721/ec4eee4fa66b6de1025bfe3a00e99f1a',
+  },
+  {
+    id: 17,
+    name: 'Wolverine',
+    photo:
+      'https://media.gq.com/photos/58b9fda8803bdb766dd69ef7/master/pass/wolverine.jpg',
+  },
+  {
+    id: 18,
+    name: 'Deadpool',
+    photo: 'https://miro.medium.com/max/3840/1*0ubYRV_WNR9iYrzUAVINHw.jpeg',
+  },
+  {
+    id: 19,
+    name: 'Flash',
+    photo:
+      'https://www.iguides.ru/upload/iblock/e4c/e4ca34ddd972e7d47e0b3378d2dbfad3.jpg',
+  },
+  {
+    id: 20,
+    name: 'Joker',
+    photo:
+      'https://img5.goodfon.ru/wallpaper/nbig/c/b9/joaquin-phoenix-khoakin-feniks-dzhoker-dzhoker-2019-joker-20.jpg',
+  },
 ];
